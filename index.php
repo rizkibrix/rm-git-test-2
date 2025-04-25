c537473c8003271741a06299004a64678db3b18a
rizkibrix <rizkimlna25@gmail.com>
2025-04-25 16:16:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cum debitis cumque explicabo obcaecati ipsum laboriosam corporis accusantium. Reprehenderit, illum?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
